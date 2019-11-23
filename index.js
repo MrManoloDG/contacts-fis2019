698183a9e0275d166c22e095d0e623e60c9d9b64
Manuel <ManuelDiazGil96@hotmail.com>
2019-11-23 11:23:21 +0000

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -4,26 +4,20 @@ const Datastore = require('nedb')
 const app = express()
 const port = 8000;
 
-var db = new Datastore({ filename: './datastore/data', autoload: true });
+var BASE_API_PATH = "/api/v1";
+var DB_FILE_NAME = __dirname + "/datastore/data";
+
+var db = new Datastore({ filename: DB_FILE_NAME, autoload: true });
+
 app.use(bodyParser.json());
 
-var BASE_API_PATH = "/api/v1";
 
-var contacts = [
-    {
-        "name":"Peter",
-        "phone":"654789123"
-    },
-    {
-        "name":"Jhon",
-        "phone":"612345678"
-    }
-]
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get(BASE_API_PATH + "/contacts", (req,res) => {
     console.log(Date() + " - GET /contacts");
+
     db.find({},{ name: 1, phone: 1, _id: 0 }, function (err, docs) {
         res.send(docs);
     });
@@ -32,8 +26,14 @@ app.get(BASE_API_PATH + "/contacts", (req,res) => {
 app.post(BASE_API_PATH + "/contacts", (req,res) => {
     console.log(Date() + " - POST /contacts");
     let contact = req.body;
-    db.insert(contact);
-    res.sendStatus(201);
+    db.insert(contact, (err) => {
+        if(err) {
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(201);
+        }
+    });
 });
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
